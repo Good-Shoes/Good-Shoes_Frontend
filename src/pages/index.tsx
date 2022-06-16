baef4d0af20c1ce3bc72fd933b4e9d76bd019305
mingyeongmo <mingyeongmp@naver.com>
2022-06-16 18:19:04 +0900

    한번에 컴포넌트들을 import 하기 위한 파일

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,14 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Dra_board from "../components/Board/drawing_board/Dra_board";
-// import Eraser from "../components/Drawing_mode/eraser/Eraser";
-// import Pencil from "../components/Drawing_mode/pencil/Pencil";
 import { Refresh, Pencil, Eraser } from "../components/Drawing_mode";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
 import { Mouse_Cursor } from "../Atoms/atom";
 import * as S from "./indexStyle";
 import View_board from "../components/Board/view_board/View_board";
 
 const Home: NextPage = () => {
   const Cursor = useRecoilValue(Mouse_Cursor);
-  const setCursor = useSetRecoilState(Mouse_Cursor);
 
   return (
     <>
@@ -25,7 +22,6 @@ const Home: NextPage = () => {
           <Pencil />
           <Eraser />
         </S.Cursor>
-
         <S.Board>
           <Dra_board />
           <View_board />
