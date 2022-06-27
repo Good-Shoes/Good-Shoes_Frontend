a8c3392e2ac9831be6d48ac5d5f4296ea03641cb
mingyeongmo <mingyeongmp@naver.com>
2022-06-27 17:51:32 +0900

    버튼 클릭시 comfirm 창 뜨게 하기

--- src/components/ChangeButton/Change_button.tsx
+++ src/components/ChangeButton/Change_button.tsx
@@ -2,7 +2,19 @@ import React from "react";
 import { Button } from "./Style";
 
 const Change_button = () => {
-  return <Button>버튼입니다.</Button>;
+  const Convert = () => {
+    let convert = confirm("당신만의 신발을 만드시겠습니까?");
+    if (convert) {
+      alert("만들었습니다.");
+    } else {
+      alert("취소하였습니다.");
+    }
+  };
+  return (
+    <Button onClick={Convert}>
+      <p>변환하기</p>
+    </Button>
+  );
 };
 
 export default Change_button;
--- src/components/ChangeButton/Style.ts
+++ src/components/ChangeButton/Style.ts
@@ -4,4 +4,16 @@ export const Button = styled.div`
   grid-row-start: 3;
   grid-column-start: 2;
   justify-self: center;
+  width: 150px;
+  height: 50px;
+  border: 1px solid black;
+  border-radius: 10px;
+  cursor: pointer;
+
+  p {
+    text-align: center;
+    line-height: 50px;
+
+    font-size: 1.5rem;
+  }
 `;
