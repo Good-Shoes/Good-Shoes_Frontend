dd65a8e41a302c6503737ee33d9e3b87ff610a6c
mingyeongmo <mingyeongmp@naver.com>
2022-07-01 16:07:45 +0900

    Blob 함수 이용해서 보낼 데이터 파일 만들기

--- src/components/ChangeButton/Change_button.tsx
+++ src/components/ChangeButton/Change_button.tsx
@@ -2,12 +2,40 @@ import React from "react";
 import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 import { DataUrl } from "../../Atoms/atom";
-
+import axios from "axios";
 const Change_button = () => {
   const Image = useRecoilValue(DataUrl);
   const Convert = () => {
-    console.log(Image.toDataURL("image/png"));
-    // canvas.toDataURL("image/png")
+    const imgUrl = Image.toDataURL("image/png");
+    console.log(imgUrl);
+    dataURLtoFile(imgUrl);
+  };
+
+  const dataURLtoFile = (imgURL: any) => {
+    const blobBin = window.atob(imgURL.split(",")[1]); // base64 데이터 디코딩
+    const array = [];
+
+    for (let i = 0; i < blobBin.length; i++) {
+      array.push(blobBin.charCodeAt(i)); // 인코드된 문자들을 0번째부터 끝까지 해독하여 유니코드 값을 array 에 저장한다.
+    }
+
+    const u8arr = new Uint8Array(array);
+    const file = new Blob([u8arr], { type: "shoes.png" });
+    const formdata = new FormData();
+    formdata.append("ShoesImg", file);
+    console.log(file);
+
+    axios
+      .post("", formdata, {
+        headers: {},
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        alert("실패");
+        console.log(err);
+      });
   };
   return (
     <Button onClick={Convert}>
